d310fd536888501e42f8fdea05738b3ac4a9bc2f
Jordi Piella <jordipiella@gmail.com>
2018-02-07 11:04:55 +0100

    Change name

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('planmarcillaprofesional App', () => {
+describe('Angular5ExpressInSameProjectInitialDeployHeroku App', () => {
   let page: AppPage;
 
   beforeEach(() => {
